440917ae164c486a95b43a887ca73f22467177ea
mgthihazaw <mgthihazaw777@gmail.com>
2019-06-04 14:30:35 +0630

    add work section

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,16 @@
 $(document).ready(function(){
 	new WOW().init();
 })
+/*********************************************
+                            Magnific popup Image
+**********************************************/
+$(function() {
+  $('#work').magnificPopup({
+     delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  gallery: {
+      enabled: true
+    },
+ 
+  });
+});
